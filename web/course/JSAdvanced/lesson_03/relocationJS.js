b911c568a3cb28842f76eea53be69ea16255a691
CenatoRxD <deychuk93@gmail.com>
2017-07-08 11:05:29 +0200

    js_advanced_03

---
+++ web/course/JSAdvanced/lesson_03/relocationJS.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Sviatoslav on 08.07.2017.
+ */
+window.onload = function () {
+    var timer = window.setTimeout(relocate, 5000)
+};
+
+function relocate() {
+    document.location = "https://www.google.com/";
+}
